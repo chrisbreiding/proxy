82c9007cc9c7f4f80acf72434dd191a41ddab76c
Chris Breiding <chrisbreiding@gmail.com>
2022-08-14 20:33:28 -0400

    run nightly scripts serially

--- scripts/run-nightly.js
+++ scripts/run-nightly.js
@@ -1,2 +1,8 @@
-require('../lib/notion/update-restaurants-last-visit')()
-require('../lib/notion/update-date-extrapolation')()
+const { mapPromisesSerially } = require('../util/collections')
+
+const scripts = [
+  require('../lib/notion/update-restaurants-last-visit'),
+  require('../lib/notion/update-date-extrapolation'),
+]
+
+mapPromisesSerially(scripts, (script) => script())
