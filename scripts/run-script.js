4f6ee77d79baf4089d9d9f9e3f113a93898311ad
Chris Breiding <chrisbreiding@gmail.com>
2022-10-15 10:43:05 -0400

    add dev script logging

--- scripts/run-script.js
+++ scripts/run-script.js
@@ -1,1 +1,17 @@
-require(`../lib/notion/${process.argv[2]}`)()
+/* eslint-disable no-console */
+const scriptName = process.argv[2]
+const script = require(`../lib/notion/${scriptName}`)
+
+console.log('Running dev script:', scriptName)
+
+const run = async () => {
+  try {
+    await script()
+    console.log('Successfully ran', scriptName)
+  } catch (error) {
+    console.log('Running', scriptName, 'failed:')
+    console.log(error.stack)
+  }
+}
+
+run()
