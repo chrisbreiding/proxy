e18adca4dba4ecc2a30f166e5e448d86dc41f8c3
Chris Breiding <chrisbreiding@gmail.com>
2022-11-03 08:28:05 -0400

    add tests for year-in-review

---
+++ scripts/random-permutations.js
@@ -0,0 +1,35 @@
+const fs = require('fs-extra')
+const minimist = require('minimist')
+const path = require('path')
+
+const { count, limit } = minimist(process.argv.slice(2))
+
+const options = [
+  't.a.a',
+  't.a.b',
+  't.a.c',
+  't.b.a',
+  't.b.b',
+  't.b.c',
+  't.c.a',
+  't.c.b',
+  't.c.c',
+  't.d.a',
+  't.d.b',
+  't.d.c',
+]
+
+const randomInt = (minimum, maxPlusOne) => {
+  return Math.floor((Math.random() * (maxPlusOne - minimum))) + minimum
+}
+
+const randomPermutations = new Array(count).fill(1).map(() => {
+  const itemCount = randomInt(1, limit + 1)
+  const perms = new Array(itemCount).fill(1).map(() => {
+    return options[randomInt(0, options.length)]
+  }).join(', ')
+
+  return `...d('', ${perms}),`
+}).join('\n')
+
+fs.writeFileSync(path.join(__dirname, '../.fixtures/permutations.txt'), `${randomPermutations}`)
---
+++ test/lib/notion/year-in-review.spec.ts
@@ -0,0 +1,46 @@
+import fs from 'fs-extra'
+import { describe, expect, it } from 'vitest'
+
+import { fixture, nockGetBlockChildren, nockAppendBlockChildren } from '../../support/util'
+import main from '../../../lib/notion/year-in-review'
+import monthBlocks from '../../fixtures/notion/year-in-review/month-blocks'
+
+describe('lib/notion/year-in-review', () => {
+  it('adds year summmary', async () => {
+    nockGetBlockChildren('done-page-id', { fixture: 'notion/year-in-review/done-blocks' })
+    nockGetBlockChildren('year-id', { fixture: 'notion/year-in-review/year-blocks' })
+    nockGetBlockChildren('january-id', { reply: monthBlocks.january })
+    nockGetBlockChildren('february-id', { reply: monthBlocks.february })
+    nockGetBlockChildren('march-id', { reply: monthBlocks.march })
+    nockGetBlockChildren('april-id', { reply: monthBlocks.april })
+    nockGetBlockChildren('may-id', { reply: monthBlocks.may })
+    nockGetBlockChildren('june-id', { reply: monthBlocks.june })
+    nockGetBlockChildren('july-id', { reply: monthBlocks.july })
+    nockGetBlockChildren('august-id', { reply: monthBlocks.january })
+    nockGetBlockChildren('september-id', { reply: monthBlocks.september })
+    nockGetBlockChildren('october-id', { reply: monthBlocks.october })
+    nockGetBlockChildren('november-id', { reply: monthBlocks.november })
+    nockGetBlockChildren('december-id', { reply: monthBlocks.december })
+
+    nockAppendBlockChildren({
+      id: 'year-id',
+      body: fs.readJsonSync(fixture('notion/year-in-review/result')),
+    })
+
+    await main.yearInReview({
+      donePageId: 'done-page-id',
+      notionToken: 'notion-token',
+      year: '2021',
+    })
+  })
+
+  it('errors if year cannot be found', async () => {
+    nockGetBlockChildren('done-page-id', { fixture: 'notion/year-in-review/done-blocks' })
+
+    await expect(() => main.yearInReview({
+      donePageId: 'done-page-id',
+      notionToken: 'notion-token',
+      year: '2025',
+    })).rejects.toThrow('Could not find page for year: 2025')
+  })
+})
