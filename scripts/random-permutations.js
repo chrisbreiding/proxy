7de5c5f79f9536885c0612273164d7a65224909e
Chris Breiding <chrisbreiding@gmail.com>
2022-11-12 22:50:39 -0500

    fix syntax error

--- scripts/random-permutations.js
+++ scripts/random-permutations.js
@@ -32,4 +32,4 @@ const randomPermutations = new Array(count).fill(1).map(() => {
   return `...d('', ${perms}),`
 }).join('\n')
 
-fs.writeFileSync(process.cwd(), '.fixtures/permutations.txt'), `${randomPermutations}`)
+fs.writeFileSync(path.join(process.cwd(), '.fixtures/permutations.txt'), `${randomPermutations}`)
