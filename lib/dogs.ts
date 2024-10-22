29007cfc8a7b26aad350966081586ebce1bbf74c
Chris Breiding <chrisbreiding@gmail.com>
2024-10-21 20:57:26 -0400

    handle hal errors gracefully

--- lib/dogs.ts
+++ lib/dogs.ts
@@ -2,18 +2,32 @@ import type express from 'express'
 import { request } from './util/network'
 
 export async function get (req: express.Request, res: express.Response) {
-  const response = await request({
-    url: `https://prod-hal-api.homeatlastdogrescue.com/dogs/${req.params.id}`,
-  })
+  try {
+    const response = await request({
+      url: `https://prod-hal-api.homeatlastdogrescue.com/dogs/${req.params.id}`,
+    })
 
-  res.json(response)
+    res.json(response)
+  } catch (error: any) {
+    res.status(500).json({
+      error,
+      data: error?.response?.data,
+    })
+  }
 }
 
 export async function search (req: express.Request, res: express.Response) {
-  const response = await request({
-    url: 'https://prod-hal-api.homeatlastdogrescue.com/dogs/search',
-    params: req.query,
-  })
+  try {
+    const response = await request({
+      url: 'https://prod-hal-api.homeatlastdogrescue.com/dogs/search',
+      params: req.query,
+    })
 
-  res.json(response)
+    res.json(response)
+  } catch (error: any) {
+    res.status(500).json({
+      error,
+      data: error?.response?.data,
+    })
+  }
 }
--- test/lib/dogs.spec.ts
+++ test/lib/dogs.spec.ts
@@ -4,6 +4,19 @@ import { describe, expect, it } from 'vitest'
 import { handleServer } from '../util'
 import { startServer } from '../../index'
 
+function createError () {
+  const error = new Error('failed') as Error & { response: { status: number, data: any } }
+
+  error.response = {
+    status: 401,
+    data: {
+      more: 'info',
+    },
+  }
+
+  return error
+}
+
 describe('lib/dogs', () => {
   describe('GET /dogs/:id', () => {
     handleServer(startServer)
@@ -18,14 +31,28 @@ describe('lib/dogs', () => {
       expect(res.status).to.equal(200)
       expect(res.body).to.deep.equal({ id: 1234 })
     })
+
+    it('sends 500 on error', async (ctx) => {
+      const error = createError()
+
+      nock('https://prod-hal-api.homeatlastdogrescue.com')
+      .get('/dogs/1234')
+      .replyWithError(error)
+
+      const res = await ctx.request.get('/dogs/1234')
+
+      expect(res.status).to.equal(500)
+      expect(res.body.error.message).to.equal('failed')
+      expect(res.body.data).to.deep.equal({ more: 'info' })
+    })
   })
 
   describe('GET /dogs', () => {
+    const query = '?includePhotos=true'
+
     handleServer(startServer)
 
     it('returns dogs from HAL', async (ctx) => {
-      const query = '?includePhotos=true'
-
       nock('https://prod-hal-api.homeatlastdogrescue.com')
       .get(`/dogs/search${query}`)
       .reply(200, { results: [] })
@@ -34,6 +61,20 @@ describe('lib/dogs', () => {
 
       expect(res.status).to.equal(200)
       expect(res.body).to.deep.equal({ results: [] })
+    })
+
+    it('sends 500 on error', async (ctx) => {
+      const error = createError()
+
+      nock('https://prod-hal-api.homeatlastdogrescue.com')
+      .get(`/dogs/search${query}`)
+      .replyWithError(error)
+
+      const res = await ctx.request.get(`/dogs${query}`)
+
+      expect(res.status).to.equal(500)
+      expect(res.body.error.message).to.equal('failed')
+      expect(res.body.data).to.deep.equal({ more: 'info' })
     })
   })
 })
