2e125f23f14d03ca0bb7d79453bf6c281a61576c
Chris Breiding <chrisbreiding@gmail.com>
2026-02-08 16:12:11 -0500

    actually fix env issue

--- lib/util/env.ts
+++ lib/util/env.ts
@@ -1,15 +1,17 @@
 import { readJsonSync } from 'fs-extra'
+import minimist from 'minimist'
 
 type EnvFile = { [key: string]: any }
 
-const isDevelopment = process.env.NODE_ENV === 'development'
-const isTest = process.env.TEST === 'true'
+const args = minimist(process.argv.slice(2)) as { test?: boolean }
+const isDevelopmentEnv = process.env.NODE_ENV === 'development'
+const isLocalTest = !!args.test
 /* v8 ignore next -- @preserve */
-const envFile: EnvFile = isDevelopment || isTest ? readJsonSync('./.env') : {}
+const envFile: EnvFile = isDevelopmentEnv || isLocalTest ? readJsonSync('./.env') : {}
 
 export function getEnv (key: string): string | undefined {
   /* v8 ignore next -- @preserve */
-  const testValue = isTest ? envFile[`TEST_${key}`] : undefined
+  const testValue = isLocalTest ? envFile[`TEST_${key}`] : undefined
   const devValue = envFile[key]
   const prodValue = process.env[key]
 
